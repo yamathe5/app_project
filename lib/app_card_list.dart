f0d92a902f6dbbbcd95401f1cf2f9f139e87dc45
yamathe5Intermet <manuelsegmar@gmail.com>
2023-02-01 17:37:22 -0500

    chore

--- lib/app_card_list.dart
+++ lib/app_card_list.dart
@@ -1,59 +1,59 @@
-import 'package:flutter/material.dart';
-import 'app_card.dart';
+// import 'package:flutter/material.dart';
+// // import 'app_card.dart';
 
-class AppCardList extends StatelessWidget {
-  const AppCardList({super.key});
+// class AppCardList extends StatelessWidget {
+//   const AppCardList({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    var json = [
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      },
-      {
-        "title": "Diplomado en piping",
-        "Desdripcion":
-            "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
-      }
-    ];
+//   @override
+//   Widget build(BuildContext context) {
+//     var json = [
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       },
+//       {
+//         "title": "Diplomado en piping",
+//         "Desdripcion":
+//             "Se trata de un diplomado teórico-práctico donde se expondrán los principales conocimientos para el diseño de tuberías, que un profesional del sector tiene que dominar para poder desarrollar su trabajo."
+//       }
+//     ];
 
-    return SingleChildScrollView(child: Text(json[0].title));
-  }
-}
+//     return SingleChildScrollView(child: Text(json[0].title));
+//   }
+// }
